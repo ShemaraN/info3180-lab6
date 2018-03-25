12b1af8eb2cef36d34a752754635e53bbb59ce2c
Shemii N <shemaranicky@hotmail.com>
2018-03-25 08:15:02 +0000

    Added News Component

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -39,6 +39,17 @@ Vue.component('app-footer', {
     }
 })
 
+Vue.component('app-news-list', {
+    template:`
+        <div class="news">
+        <h2>News</h2>
+        <ul class="news__list">
+        <li class="news__item">News item 1</li>
+        <li class="news__item">News item 2</li>
+        <li class="news__item">News item 3</li>
+        </ul>
+        </div>    `
+})
 
 let app = new Vue({
     el: '#app',
